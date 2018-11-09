02cfa38e9e7f8c825ef3de7422fef32313310053
ojha-url <mayank.ojha@gmail.com>
2018-11-09 17:44:53 -0500

    Added review card child components

---
+++ src/app/components/report-review/report-review.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-report-review',
+  templateUrl: './report-review.component.html',
+  styleUrls: ['./report-review.component.scss']
+})
+export class ReportReviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/routes/cards/review/review.module.ts
+++ src/app/routes/cards/review/review.module.ts
@@ -4,12 +4,13 @@ import { CommonModule } from '@angular/common';
 import { ReviewRoutingModule } from './review-routing.module';
 import { ReviewComponent } from './review.component';
 import { SubmitSwipeComponent } from '../../../components/submit-swipe/submit-swipe.component';
+import { ReportReviewComponent } from '../../../components/report-review/report-review.component';
 
 @NgModule({
   imports: [
     CommonModule,
     ReviewRoutingModule
   ],
-  declarations: [ReviewComponent, SubmitSwipeComponent]
+  declarations: [ReviewComponent, SubmitSwipeComponent, ReportReviewComponent]
 })
 export class ReviewModule { }
