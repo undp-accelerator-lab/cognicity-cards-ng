cc7f08f45602ea057e81721e461ccb8daf3f3b23
IlhamWahabiGX <wahabiputra@gmail.com>
2018-12-29 09:13:20 +0700

    Able to show airquality facts

--- src/app/routes/cards/airquality/airquality.component.ts
+++ src/app/routes/cards/airquality/airquality.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-airquality',
   templateUrl: './airquality.component.html',
   styleUrls: ['./airquality.component.scss']
 })
-export class AirqualityComponent implements OnInit {
+export class AirqualityComponent {
+
+  facts: string[] = [
+    "None",
+    "Tercium Bau Asap",
+    "Mata Pedih / Radang Tenggorokan",
+    "Pusing / Mual",
+    "Sesak Nafas / Dada Nyeri"
+  ]
+
+  fact: string = this.facts[2]
 
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
