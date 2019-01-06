f3ff559ceae3565d8e36cd13e3378ab3a72d5e86
IlhamWahabiGX <wahabiputra@gmail.com>
2019-01-06 21:28:04 +0700

    Fix hovering firetype, add basic mixins

--- src/app/routes/cards/firetype/firetype.component.ts
+++ src/app/routes/cards/firetype/firetype.component.ts
@@ -1,34 +1,8 @@
 import { Component } from '@angular/core';
-import { Event } from '@angular/router';
 
 @Component({
   selector: 'app-firetype',
   templateUrl: './firetype.component.html',
   styleUrls: ['./firetype.component.scss']
 })
-export class FiretypeComponent {
-
-  constructor() { }
-
-  fireButtonImage = "../../../../assets/decks/fire/firetype/AddFireReport.png"
-  hazeButtonImage = "../../../../assets/decks/fire/firetype/AddHazeReport.png"
-
-  highlightImage(event: MouseEvent, type: string) {
-    switch (type) {
-      case "fire": {
-        this.fireButtonImage === "../../../../assets/decks/fire/firetype/AddFireReport.png" 
-          ? this.fireButtonImage = "../../../../assets/decks/fire/firetype/AddFireReport_Highlight.png" 
-          : this.fireButtonImage = "../../../../assets/decks/fire/firetype/AddFireReport.png";
-        break; 
-      }
-      case "haze": {
-        this.hazeButtonImage === "../../../../assets/decks/fire/firetype/AddHazeReport.png" 
-          ? this.hazeButtonImage = "../../../../assets/decks/fire/firetype/AddHazeReport_Highlight.png" 
-          : this.hazeButtonImage = "../../../../assets/decks/fire/firetype/AddHazeReport.png";
-        break;
-      }
-      default: break;
-    }
-  }
-
-}
+export class FiretypeComponent {}
