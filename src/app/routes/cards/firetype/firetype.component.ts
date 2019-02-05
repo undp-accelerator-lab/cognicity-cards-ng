60aa3da7bbc0a84b42e35a9d650a1f460572a29e
IlhamWahabiGX <wahabiputra@gmail.com>
2019-02-05 23:39:47 +0700

    Use deckservice in firetype

--- src/app/routes/cards/firetype/firetype.component.ts
+++ src/app/routes/cards/firetype/firetype.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavigationService } from '../../../services/navigation.service';
 import { ActivatedRoute } from '@angular/router';
+import { DeckService } from '../../../services/cards/deck.service'
 
 @Component({
   selector: 'app-firetype',
@@ -10,6 +11,12 @@ import { ActivatedRoute } from '@angular/router';
 export class FiretypeComponent {
   constructor(
     public navController: NavigationService,
-    public route: ActivatedRoute
-  ){}
+    public route: ActivatedRoute,
+    public deckService: DeckService,
+  ) { }
+
+  onTypeSelected(type: string) {
+    this.deckService.setDeckType(type)
+    this.navController.next(this.route)
+  }
 }
