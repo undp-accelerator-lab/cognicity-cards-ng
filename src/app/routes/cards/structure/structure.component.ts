712e2cae9c69b26e82285a109f4657858b54d8aa
IlhamWahabiGX <wahabiputra@gmail.com>
2019-03-20 14:53:26 +0700

    Add slider arrow to structure

--- src/app/routes/cards/structure/structure.component.ts
+++ src/app/routes/cards/structure/structure.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewChecked, ChangeDetectorRef } from '@angular/core';
 import { StructureService } from '../../../services/cards/earthquake/structure.service';
 
 @Component({
@@ -6,7 +6,7 @@ import { StructureService } from '../../../services/cards/earthquake/structure.s
   templateUrl: './structure.component.html',
   styleUrls: ['./structure.component.scss']
 })
-export class StructureComponent {
+export class StructureComponent implements AfterViewChecked {
   titles = [
     "Cracking",
     "Partially Collapsed",
@@ -24,18 +24,46 @@ export class StructureComponent {
   structure: number
 
   constructor(
-    public structureService: StructureService
-  ) {
-    this.setStructureFailure(structureService.getStructureFailure())
+    public structureService: StructureService,
+    private cdRef: ChangeDetectorRef
+  ) {}
+
+  ngAfterViewChecked() {
+    this.setStructureFailure(this.structureService.getStructureFailure())
+    this.cdRef.detectChanges()
   }
 
   public setStructureFailure(value): void {
     const intValue = parseInt(value)
+    const leftArrow = document.querySelector('.left-arrow') as HTMLDivElement
+    const rightArrow = document.querySelector('.right-arrow') as HTMLDivElement
+    const slider = document.querySelector('.structure__slider-range') as HTMLInputElement
 
     this.structure = intValue
     this.title = this.titles[intValue]
     this.image = this.images[intValue]
 
     this.structureService.setStructureFailure(intValue)
+    leftArrow.style.left = this.countArrowOffset(intValue, slider.offsetWidth, 'left')
+    rightArrow.style.left = this.countArrowOffset(intValue, slider.offsetWidth, 'right')
+  }
+
+  private countArrowOffset(
+    inputValue: number, 
+    sliderWidth: number, 
+    type: 'left' | 'right'
+  ): string {
+    const circleThumbDiameter = 25 //px
+
+    const circleThumbRadius = circleThumbDiameter / 2
+    const circleThumbPosition = inputValue * sliderWidth / 2
+
+    const arrowOffsetRelative = (type === 'left' ? 
+      circleThumbRadius - circleThumbDiameter : 
+      circleThumbRadius + circleThumbDiameter
+    )
+    const arrowOffsetAbsolute = circleThumbRadius * inputValue
+
+    return `${arrowOffsetRelative + circleThumbPosition - arrowOffsetAbsolute}px`
   }
 }
