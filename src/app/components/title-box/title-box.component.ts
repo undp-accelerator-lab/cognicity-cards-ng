a0f466cefb27050db572ebe05b296bf331a6a8c3
iwgx <wahabiputra@gmail.com>
2019-03-31 16:35:11 +0700

    Fix wind (non-branched deck) title box appear

--- src/app/components/title-box/title-box.component.ts
+++ src/app/components/title-box/title-box.component.ts
@@ -9,16 +9,33 @@ export class TitleBoxComponent implements OnInit {
   @Input() title: string;
   @Input() tabs: number[];
   @Input() color: string;
+  @Input() deck: string;
   totalTabs: number[];
 
   constructor() { }
 
   getCssClass(i) {
+    if (this.deck === 'wind') return i <= this.tabs[1] ? 'tabs filled ' : 'tabs'
     return i < this.tabs[1] ? 'tabs filled ' : 'tabs'
   }
 
+  get isShowTabs(): boolean {
+    if (this.deck === 'wind') {
+      return this.tabs[1] < this.totalTabs.length
+    }
+    return this.tabs[1] > 0 && this.tabs[1] <= this.totalTabs.length
+    
+  }
+
   ngOnInit() {
-    this.totalTabs = Array(this.tabs[0] - 2)
+    let totalTabs;
+    if (this.deck === 'wind') {
+      totalTabs = this.tabs[0] - 1
+    } else {
+      totalTabs = this.tabs[0] - 2
+    }
+
+    this.totalTabs = Array(totalTabs)
     .fill(1).map((x, i) => x + i);
   }
 
