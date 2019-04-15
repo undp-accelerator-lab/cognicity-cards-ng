824a98423d4e4485b0f0184ffd8d16a04d22c9b9
iwgx <wahabiputra@gmail.com>
2019-04-16 05:44:30 +0700

    Get card type from deck service instead

--- src/app/components/title-box/title-box.component.ts
+++ src/app/components/title-box/title-box.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { NavigationService } from '../../services/navigation.service';
+import { DeckService } from '../../services/cards/deck.service';
 
 @Component({
   selector: 'app-title-box',
@@ -8,17 +9,18 @@ import { NavigationService } from '../../services/navigation.service';
 })
 export class TitleBoxComponent {
   @Input() title: string;
-  @Input() deck: string;
 
-  constructor(public navController: NavigationService) { }
+  constructor(
+    public navController: NavigationService,
+    public deckService: DeckService) { }
 
   getCssClass(i) {
-    if (this.deck === 'earthquake') return i < this.navController.tabs[1] ? 'tabs filled ' : 'tabs'
+    if (this.deckService.getDeckType() === 'earthquake') return i < this.navController.tabs[1] ? 'tabs filled ' : 'tabs'
     return i <= this.navController.tabs[1] ? 'tabs filled ' : 'tabs'
   }
 
   get isShowTabs(): boolean {
-    if (this.deck === 'earthquake') {
+    if (this.deckService.getDeckType() === 'earthquake') {
       return this.navController.tabs[1] > 0 && this.navController.tabs[1] <= this.totalTabs.length
     }
     return this.navController.tabs[1] < this.totalTabs.length
@@ -26,7 +28,7 @@ export class TitleBoxComponent {
 
   get totalTabs(): number[] {
     let offset;
-    if (this.deck === 'earthquake') offset = 2
+    if (this.deckService.getDeckType() === 'earthquake') offset = 2
     else offset = 1
 
     return Array(this.navController.tabs[0] - offset).fill(1).map((x, i) => x + i);
