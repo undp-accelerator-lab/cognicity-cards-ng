ab236ff8288b23ec06841265e24ca1bd3597ba04
iwgx <wahabiputra@gmail.com>
2019-04-30 12:06:53 +0700

    Able to select sign

--- src/app/routes/cards/sign/sign.component.ts
+++ src/app/routes/cards/sign/sign.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign.component.scss']
 })
 export class SignComponent implements OnInit {
+  selectedOption = []
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
+  onOptionClick(option: number) {
+    if (this.selectedOption.includes(option)) {
+      this.selectedOption = this.selectedOption.filter(o => o !== option)
+    } else {
+      this.selectedOption.push(option)
+    }
+  }
 }
