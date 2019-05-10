5063da82d5d9630957ddeb63f0a1d3ec94e9ee51
iwgx <wahabiputra@gmail.com>
2019-05-11 02:56:09 +0700

    Turn swipe button to blue

--- src/app/components/submit-swipe/submit-swipe.component.ts
+++ src/app/components/submit-swipe/submit-swipe.component.ts
@@ -7,17 +7,5 @@ import { DeckService } from '../../services/cards/deck.service';
   styleUrls: ['./submit-swipe.component.scss']
 })
 export class SubmitSwipeComponent {
-  color: string
-  isMobile: boolean
-
-  constructor(
-    public deckService: DeckService
-  ) {
-    switch(deckService.getDeckType()) {
-      case 'fire': this.color = 'white'; break;
-      case 'earthquake': this.color = 'orange'; break;
-      case 'wind': this.color = 'cyan'; break;
-      default: this.color = 'blue'
-    }
-  }
+  constructor() {}
 }
