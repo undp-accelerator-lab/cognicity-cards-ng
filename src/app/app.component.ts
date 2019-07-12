55715e32a92c3a4b8e0f099024ab13246c26301c
Ilham Wahabi <wahabiputra@gmail.com>
2019-07-12 13:51:15 +0700

    Enable fullscreen in mobile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  showFullScreen() {
+    const body = document.documentElement as any;
+    if (body.requestFullscreen) {
+      body.requestFullscreen();
+    } else if (body.webkitrequestFullscreen) {
+      body.webkitrequestFullscreen();
+    } else if (body.mozrequestFullscreen) {
+      body.mozrequestFullscreen();
+    } else if (body.msrequestFullscreen) {
+      body.msrequestFullscreen();
+    }
+  }
+
+  constructor() {
+    window.addEventListener('click', () => {
+      this.showFullScreen()
+    })
+  }
 }
