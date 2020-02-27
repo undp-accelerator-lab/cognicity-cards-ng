852d3eec1176bdabde8765a8e7383e12922c710b
DC <eopoxf@gmail.com>
2020-02-27 17:57:29 +0530

    cleanup

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, NgModule } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpClientModule, HttpEventType } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { environment as env } from '../../environments/environment';
 
 @Injectable({
@@ -34,40 +34,40 @@ export class AuthService {
     var error_settings;
     var is_unique = false;
     try {
-      var responseData  = await self.GetOTLuniqueness(unique_id);
+      var responseData = await self.GetOTLuniqueness(unique_id);
       console.log(responseData);
       is_unique = true;
-      if(responseData.result.received === true)
+      if (responseData.result.received === true)
         is_unique = false;
-    } catch(error) {
+    } catch (error) {
       console.log(error);
       is_unique = false;
     }
     return is_unique;
-      // .map(response => response.json())
-      // .subscribe(
-      //   data => {
-      //     console.log(data);
-      //     is_unique = true;
-      //     // if (data.result.received === true) {
-      //     //   // card already exists
-      //     //   // error_settings.msg = self.reportcard.locale.card_error_messages.already_received;
-      //     //   return false;
-      //     // } else {
-      //     //   // populate network property of reportcard, accessed in thanks card
-      //     //   // self.reportcard.network = msg.result.network;
-      //     //   // proceed to first card
-      //     //   return true;
-      //     // }
-      //   },
-      //   err => { console.error(err) },
-      //   () => {
-      //     console.log('done checking uniqeid')
-      //     return is_unique;
-      //   }
-      // );
+    // .map(response => response.json())
+    // .subscribe(
+    //   data => {
+    //     console.log(data);
+    //     is_unique = true;
+    //     // if (data.result.received === true) {
+    //     //   // card already exists
+    //     //   // error_settings.msg = self.reportcard.locale.card_error_messages.already_received;
+    //     //   return false;
+    //     // } else {
+    //     //   // populate network property of reportcard, accessed in thanks card
+    //     //   // self.reportcard.network = msg.result.network;
+    //     //   // proceed to first card
+    //     //   return true;
+    //     // }
+    //   },
+    //   err => { console.error(err) },
+    //   () => {
+    //     console.log('done checking uniqeid')
+    //     return is_unique;
+    //   }
+    // );
   }
-  GetOTLuniqueness(uniqueId) : Promise<any> {
+  GetOTLuniqueness(uniqueId): Promise<any> {
     return this.http.get(env.data_server + 'cards/' + uniqueId).toPromise();
   }
 }
