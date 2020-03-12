77c833aac4704137fa55dfdc4add7a026bd2cf91
DC <eopoxf@gmail.com>
2020-03-12 16:30:12 +0530

    Set default language of flood card to Bahasa

--- src/app/routes/decks/flood/flood.component.ts
+++ src/app/routes/decks/flood/flood.component.ts
@@ -19,10 +19,10 @@ export class FloodComponent implements OnInit {
     public deckService: DeckService
   ) {
     // this language will be used as a fallback when a translation isn't found in the current language
-    translate.setDefaultLang('en');
+    translate.setDefaultLang('id');
 
     // the lang to use, if the lang isn't available, it will use the current loader to get them
-    translate.use('en');
+    translate.use('id');
 
     // Store card routes for navigation
     this.navController.registerCardRoutes('flood');
