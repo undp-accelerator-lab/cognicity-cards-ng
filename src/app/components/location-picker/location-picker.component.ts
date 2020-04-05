529ebe9f94de87829f3076b88f0b6eb73977a988
DC <eopoxf@gmail.com>
2020-04-05 22:08:13 +0530

    Restrict location pick to Indonesia

--- src/app/components/location-picker/location-picker.component.ts
+++ src/app/components/location-picker/location-picker.component.ts
@@ -91,6 +91,7 @@ export class LocationPickerComponent implements OnInit {
   }
 
   async onSearch(query: string) {
+    query = query + ", Indonesia";
     const results = await this.provider.search({ query });
     this.searchResults = results; //we send this to the child component search-location
   }
