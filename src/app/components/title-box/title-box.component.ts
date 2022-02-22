51996a7ec13e9e1edcadfc2cebc0982e08209160
Deepthi Chand <eopoxf@gmail.com>
2022-02-22 14:17:37 +0530

    Add basic flow to capture partner code

--- src/app/components/title-box/title-box.component.ts
+++ src/app/components/title-box/title-box.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { NavigationService } from '../../services/navigation.service';
 import { DeckService } from '../../services/cards/deck.service';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-title-box',
@@ -9,28 +10,41 @@ import { DeckService } from '../../services/cards/deck.service';
 })
 export class TitleBoxComponent {
   @Input() title: string;
+  partnerCode = '';
 
   constructor(
     public navController: NavigationService,
     public deckService: DeckService) { }
 
   getCssClass(i) {
-    if (this.deckService.getDeckType() === 'earthquake') return i < this.navController.tabs[1] ? 'tabs filled ' : 'tabs'
-    return i <= this.navController.tabs[1] ? 'tabs filled ' : 'tabs'
+    if (this.deckService.getDeckType() === 'earthquake') { return i < this.navController.tabs[1] ? 'tabs filled ' : 'tabs'; }
+    return i <= this.navController.tabs[1] ? 'tabs filled ' : 'tabs';
   }
 
   get isShowTabs(): boolean {
     if (this.deckService.getDeckType() === 'earthquake') {
-      return this.navController.tabs[1] > 0 && this.navController.tabs[1] <= this.totalTabs.length
+      return this.navController.tabs[1] > 0 && this.navController.tabs[1] <= this.totalTabs.length;
     }
-    return this.navController.tabs[1] < this.totalTabs.length
+    return this.navController.tabs[1] < this.totalTabs.length;
   }
 
   get totalTabs(): number[] {
     let offset;
-    if (this.deckService.getDeckType() === 'earthquake') offset = 2
-    else offset = 1
+    if (this.deckService.getDeckType() === 'earthquake') { offset = 2; } else { offset = 1; }
 
     return Array(this.navController.tabs[0] - offset).fill(1).map((x, i) => x + i);
+  }
+
+  closePartnerPopup() {
+    $('#partnerCodePopup').hide();
+  }
+
+  showPartnerPopup() {
+    $('#partnerCodePopup').show();
+  }
+
+  submitCode() {
+    this.deckService.setPartnerCode(<string>$('#partnerCode').val());
+    this.closePartnerPopup();
   }
 }
