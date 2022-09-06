5328660c3b68f2b45f8593d34705b7f068edcb6b
ruthvik129 <ruthvik129@gmail.com>
2022-09-06 11:03:20 +0530

    Add mapbox changes to fire

--- src/app/routes/cards/firedistance/firedistance.component.ts
+++ src/app/routes/cards/firedistance/firedistance.component.ts
@@ -3,9 +3,10 @@ import { OpenStreetMapProvider } from 'leaflet-geosearch';
 
 import { DeckService } from '../../../services/cards/deck.service'
 import { MONUMEN_NASIONAL_LAT_LNG } from "../../../../utils/const";
+import mapboxgl from 'mapbox-gl';
+import { lineString , length} from "@turf/turf";
 
 declare let L;
-
 @Component({
   selector: "app-firedistance",
   templateUrl: "./firedistance.component.html",
@@ -17,7 +18,9 @@ export class FiredistanceComponent implements OnInit {
   private informerMarker;
   private fireMarker;
   private map;
+  private geojson;
   private distanceLine;
+  private labelLayer;
   private provider;
   public search;
 
@@ -40,36 +43,75 @@ export class FiredistanceComponent implements OnInit {
 
   private initMap() {
     let { lat, lng } = MONUMEN_NASIONAL_LAT_LNG
+    console.log("🚀 ~ file: firedistance.component.ts ~ line 49 ~ FiredistanceComponent ~ initMap ~ this.deckService.getLocation()", this.deckService.getLocation())
 
     if (this.deckService.getFireLocation()) {
       lat = this.deckService.getLocation().lat
       lng = this.deckService.getLocation().lng
     }
 
-    this.map = L.map('mapid', { 
-      center: [ lat, lng ],
-      zoom: 16
+    // const accessToken = 'pk.eyJ1IjoiaWxoYW13YWhhYmkiLCJhIjoiY2p5MGllYW96MDNoNjNobnF2cWh2c3dkZyJ9.Vfmf0KAT-gZBA4L2LF7PNg';
+    
+    // L.tileLayer(`https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}?access_token=${accessToken}`, {
+    //   attribution: '© <a href="https://www.mapbox.com/feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    // }).addTo(this.map);
+
+    // // Add compass
+    // this.map.addControl(new L.Control.Compass());
+
+    // // Check if there is any error (ex: user turn off geolocation)
+    // this.checkGeolocation()
+
+    // // Add locate
+    // const locate = L.control.locate({ icon: 'locate', keepCurrentZoomLevel: true }).addTo(this.map);
+
+    // if (this.deckService.getLocation()) this.onLocateFound()
+    // else locate.start() // Locate user current location
+
+
+    mapboxgl.accessToken = 'pk.eyJ1IjoicGV0YWJlbmNhbmEiLCJhIjoiY2s2MjF1cnZmMDlxdzNscWc5MGVoMTRkeCJ9.PGcoQqU6lBrcLfBmvTrWrQ';
+    this.map = new mapboxgl.Map({
+        container: 'mapid', // container ID
+        style: 'mapbox://styles/mapbox/streets-v11', // style URL
+        center:[lng , lat], // starting position [lng, lat]
+        zoom: 16, // starting zoom
     });
 
-    const accessToken = 'pk.eyJ1IjoiaWxoYW13YWhhYmkiLCJhIjoiY2p5MGllYW96MDNoNjNobnF2cWh2c3dkZyJ9.Vfmf0KAT-gZBA4L2LF7PNg';
+    const self = this
+
+    this.geojson = {
+      'type': 'FeatureCollection',
+      'features': []
+      };
+       
     
-    L.tileLayer(`https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}?access_token=${accessToken}`, {
-      attribution: '© <a href="https://www.mapbox.com/feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(this.map);
+       
+      this.map.on('load', () => {
+      this.map.addSource('geojson', {
+      'type': 'geojson',
+      'data': this.geojson
+      });
+       
+      this.map.addLayer({
+      id: 'measure-lines',
+      type: 'line',
+      source: 'geojson',
+      layout: {
+      'line-cap': 'round',
+      'line-join': 'round'
+      },
+      paint: {
+      'line-color': 'red',
+      'line-width': 2.5
+      },
+      filter: ['in', '$type', 'LineString']
+      });
+      self.onLocateFound()
+    })
 
-    // Add compass
-    this.map.addControl(new L.Control.Compass());
+    // else locate.start() // Locate user current location
 
-    // Check if there is any error (ex: user turn off geolocation)
-    this.checkGeolocation()
-
-    // Add locate
-    const locate = L.control.locate({ icon: 'locate', keepCurrentZoomLevel: true }).addTo(this.map);
-
-    if (this.deckService.getLocation()) this.onLocateFound()
-    else locate.start() // Locate user current location
-
-    this.map.on("locationfound ", () => this.onLocateFound() );
+    // this.addMarker()
 
     this.provider = new OpenStreetMapProvider();
   }
@@ -92,11 +134,7 @@ export class FiredistanceComponent implements OnInit {
     this.addDistanceLine();
   }
 
-  private countDistance(): string {
-    const distanceInMeter = this.map.distance(
-      this.fireMarker.getLatLng(),
-      this.informerMarker.getLatLng()
-    );
+  private countDistance(distanceInMeter): Number {
     const ceiledDistance = Math.ceil(distanceInMeter);
 
     let distanceInString;
@@ -110,20 +148,39 @@ export class FiredistanceComponent implements OnInit {
   }
 
   private addDistanceLine(): void {
-    if (this.distanceLine) this.distanceLine.remove(this.map);
+    if (this.distanceLine) {
+      this.geojson.features.pop()
+      this.map.getSource('geojson').setData(this.geojson)
+    }
+    if(this.labelLayer ) this.map.removeLayer('symbols')
+    const lngLat = this.fireMarker.getLngLat()
+    const infolngLat = this.informerMarker.getLngLat()
 
-    this.distanceLine = L.polyline(
-      [this.fireMarker.getLatLng(), this.informerMarker.getLatLng()],
-      {
-        color: "red"
-      }
-    ).addTo(this.map);
-
-    this.distanceLine.setText(this.countDistance(), {
-      repeat: false,
-      offset: 12,
-      center: true
-    });
+    var line = lineString([[lngLat.lng , lngLat.lat] , [infolngLat.lng , infolngLat.lat]]);
+    var Linelength = length(line, {units: 'meters'});
+    
+      // Used to draw a line between points
+      const linestring = {
+        'type': 'Feature',
+        'geometry': {
+        'type': 'LineString',
+        'coordinates': [[lngLat.lng , lngLat.lat] , [infolngLat.lng , infolngLat.lat]],
+        }
+        };
+        this.labelLayer = this.map.addLayer({
+          "id": "symbols",
+          "type": "symbol",
+          "source": "geojson",
+          "layout": {
+            "symbol-placement": "line-center",
+            "text-font": ["Open Sans Regular"],
+            "text-offset" : [0,1],
+            "text-field": this.countDistance(Linelength), // part 2 of this is how to do it
+            "text-size": 12.5
+          }
+        });
+       this.geojson.features.push(linestring);
+       this.distanceLine = this.map.getSource('geojson').setData(this.geojson);
   }
 
   private addMarker(type: "informer" | "fire"): void {
@@ -135,6 +192,7 @@ export class FiredistanceComponent implements OnInit {
           lng: this.map.getCenter().lng
         };
         this.deckService.setLocation(latlng);
+        console.log(this.deckService.getLocation())
         break;
       case "fire":
         if (!this.deckService.getFireLocation()) {
@@ -149,11 +207,22 @@ export class FiredistanceComponent implements OnInit {
         break;
     }
 
-    const marker = L.marker([latlng.lat, latlng.lng], {
-      icon: this.getIcon(type),
-      draggable: true,
-      opacity: 0.5,
-    });
+    const imageElement = document.createElement('div');
+    imageElement.className = 'marker';
+    imageElement.style.backgroundImage = `url(${this.getIcon(type)})`;
+    imageElement.style.width = `30px`;
+    imageElement.style.height = `60px`;
+    imageElement.style['margin-top'] = `-25px`;
+    imageElement.style['background-repeat'] = 'no-repeat';
+    imageElement.style.backgroundSize = '100%';
+ 
+      // Add markers to the map.
+      const marker = new mapboxgl.Marker({
+        element : imageElement,
+        draggable:true
+      })
+      .setLngLat([latlng.lng, latlng.lat])
+      .addTo(this.map);
 
     switch (type) {
       case "informer":
@@ -165,35 +234,35 @@ export class FiredistanceComponent implements OnInit {
         this.fireMarker = marker;
         break;
     }
-    marker.addTo(this.map);
 
     // Adjust distance line between informer and fire
-    marker.on("move", (e) => {
+    marker.on("drag", () => {
       this.deckService.userCanContinue()
-
       switch (type) {
         case "informer":
-          this.deckService.setLocation(e.latlng)
+          this.deckService.setLocation(this.informerMarker.getLngLat())
           break;
         case "fire":
-          this.deckService.setFireLocation(e.latlng)
+          this.deckService.setFireLocation(this.fireMarker.getLngLat())
           break;
       }
       this.addDistanceLine();
     });
 
+    // Not available on mapbox gljs (as on 26 Aug 2022) should update when available 
+
     // Change marker icon when drag and drop
     // change icon directly cause marker is not reactive so change opacity instead
     // not reactive = when doing drag, sometimes marker is left behind
-    marker.on("mouseover", () => {
-      marker.setOpacity(1)
-    });
-    marker.on("moveend", () => {
-      marker.setOpacity(0.75)
-    });
-    marker.on("mouseout", () => {
-      marker.setOpacity(0.75)
-    });
+    // marker.on("mouseover", () => {
+    //   marker.setOpacity(1)
+    // });
+    // marker.on("moveend", () => {
+    //   marker.setOpacity(0.75)
+    // });
+    // marker.on("mouseout", () => {
+    //   marker.setOpacity(0.75)
+    // });
   }
 
   async onSearch(query: string) {
@@ -210,12 +279,6 @@ export class FiredistanceComponent implements OnInit {
   }
 
   getIcon(type: 'informer' | 'fire') {
-    return L.icon({
-      iconUrl: `${this.rootIconDir}/Select${
-        type === "fire" ? "Fire" : ""
-        }Location_Grey.png`,
-      iconSize: [57.2 / 2, 103.5 / 2],
-      iconAnchor: [15, 50],
-    })
+    return `${this.rootIconDir}/Select${type === "fire" ? "Fire" : ""}Location_Grey.png`
   }
 }
