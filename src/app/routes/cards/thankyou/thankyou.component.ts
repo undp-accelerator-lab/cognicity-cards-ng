8de8eab65450f7cb2629b11abd9a50e5ca036504
ruthvik129 <ruthvik129@gmail.com>
2023-03-20 17:15:55 +0530

    add translations for location based notifications

--- src/app/routes/cards/thankyou/thankyou.component.ts
+++ src/app/routes/cards/thankyou/thankyou.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-thankyou',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ThankYouComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public translate: TranslateService
+  ) { }
 
   ngOnInit() {
   }
