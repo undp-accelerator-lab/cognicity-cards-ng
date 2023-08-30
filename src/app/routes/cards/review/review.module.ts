61c972f3e668064e3af80aad035a2276e22f616b
ruthvik129 <ruthvik129@gmail.com>
2023-08-30 12:24:30 +0530

    remove components related to location based

--- src/app/routes/cards/review/review.module.ts
+++ src/app/routes/cards/review/review.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 
 import { ReviewRoutingModule } from './review-routing.module';
 import { ReviewComponent } from './review.component';
-import { SubmitSwipeComponent } from '../../../components/submit-swipe/submit-swipe.component';
 import { SubmitButtonComponent } from '../../../components/submit-button/submit-button.component';
 import { ReportReviewComponent } from '../../../components/report-review/report-review.component';
 import { TranslateModule, TranslatePipe } from '@ngx-translate/core';
@@ -19,7 +18,7 @@ import { NgxCaptchaModule } from 'ngx-captcha';
     ReactiveFormsModule,
     NgxCaptchaModule
   ],
-  declarations: [ReviewComponent, SubmitSwipeComponent, SubmitButtonComponent , ReportReviewComponent],
+  declarations: [ReviewComponent, SubmitButtonComponent , ReportReviewComponent],
   exports: [
     TranslateModule
   ],
