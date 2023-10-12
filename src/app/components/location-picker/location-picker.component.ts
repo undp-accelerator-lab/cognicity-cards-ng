dcf79b7d561ea6698f208629cc8e86116493d53e
ruthvik129 <ruthvik129@gmail.com>
2023-10-12 15:43:15 +0530

    change the background color for selection

--- src/app/components/location-picker/location-picker.component.ts
+++ src/app/components/location-picker/location-picker.component.ts
@@ -34,7 +34,7 @@ export class LocationPickerComponent implements OnInit {
     ) { }
 
   ngOnInit() {
-    this.deckService.userCannotBack()
+    this.deckService.userCanBack()
 
     this.checkIsUserAbleToContinue()
 
